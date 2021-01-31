2f516e85a111071f4a20844111536ee12b2b5a8a
Hend Jannat <h.jannat@lamah.com>
2021-01-31 14:44:02 +0200

    login error state

--- src/redux/reducers/loginReducer.js
+++ src/redux/reducers/loginReducer.js
@@ -1,12 +1,13 @@
 
-const loginReducer = (state={logState:false, logData: {}} , action)=>{
+const loginReducer = (state={logData: {}, logState:false, logError: false} , action)=>{
     console.log(action.payload)
     switch (action.type){
         case 'LOGIN_INFO':
             return {...state, logData: action.payload};
         case 'CHANGE_LOG_STATE':
-            
-            return {...state, logState: action.payload};  
+            return {...state, logState: action.payload};
+        case 'LOGIN_ERROR':
+            return {...state, logError: action.payload};      
         default:
              return state;     
        
