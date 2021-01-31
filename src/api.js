5555713d8380a2b5ffc8aebb99a36008b91421c6
Hend Jannat <h.jannat@lamah.com>
2021-01-31 15:07:33 +0200

    change urls

--- src/api.js
+++ src/api.js
@@ -1,37 +1,36 @@
 import axios from 'axios';
-const urlLog = 'http://10.40.0.49:5000/api/login';
-const url = 'http://10.40.0.49:5000/api/markets';
-const urlMakani = "http://10.40.0.49:5000/api/location_info/"
 
-export const getStores = () => axios.get(url);
 
-export const filterStores = (keySearch) => axios.get(url,keySearch);
+const URL = 'http://10.40.0.49:5000/api';
 
-export const checkAddress = (address) => axios.get(urlMakani,address);
+export const getStores = () => axios.get(`${URL}/markets`);
+
+export const filterStores = (keySearch) => axios.get(`${URL}/makani`,keySearch);
+
+export const checkAddress = (address) => axios.get(`${URL}/customers/location_info/`,address);
 
 export const addStore = (newStore) => {
     const form_data = new FormData();
     for ( var key in newStore ) {
     form_data.append(key, newStore[key]);
     } 
-    axios.post(url, form_data)
+    axios.post(`${URL}/markets`, form_data)
 }
 
 export const log = (logData) =>{
-    console.log(urlLog)
+    
     const log_form_data = new FormData();
     for ( var key in logData ) {
     log_form_data.append(key, logData[key]);
     } 
-    return axios.post(urlLog, log_form_data);
+    return axios.post(`${URL}/login`, log_form_data);
 }
 
-const urlCustomer = 'http://10.40.0.49:5000/api/customers';
 export const addCustomer = (newCustomer) => {
     const form_data = new FormData();
     for ( var key in newCustomer ) {
     form_data.append(key, newCustomer[key]);
     } 
-    axios.post(urlCustomer, form_data)
+    axios.post(`${URL}/customers`, form_data)
 }
 
