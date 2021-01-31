b7f903c081ea1d8097f4450cf63e1143d870cbb6
Hend Jannat <h.jannat@lamah.com>
2021-01-31 09:31:50 +0200

    log post info

--- src/api.js
+++ src/api.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
-
+const URL = 'http://10.40.0.49:5000/api';
 const url = 'http://10.40.0.49:5000/api/markets';
 
  export const getStores = () => axios.get(url);
@@ -10,4 +10,12 @@ export const addStore = (newStore) => {
     form_data.append(key, newStore[key]);
     } 
     axios.post(url, form_data)
+}
+
+export const log = (logData) =>{
+    const log_form_data = new FormData();
+    for ( var key in logData ) {
+    log_form_data.append(key, logData[key]);
+    } 
+    axios.post(`${URL}/login`, log_form_data);
 }
