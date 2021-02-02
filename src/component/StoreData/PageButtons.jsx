fa4765d492b8695b1f1ca32ac8ffbbc934f58981
Hend Jannat <h.jannat@lamah.com>
2021-02-02 11:48:37 +0200

    CLASS TO CLASSnAME

--- src/component/StoreData/PageButtons.jsx
+++ src/component/StoreData/PageButtons.jsx
@@ -3,19 +3,19 @@ import React from 'react';
 const PageButtons = () => {
     return(
         <div className ="container">
-        <div class="ui center aligned ten column grid">
+        <div className="ui center aligned ten column grid">
             <div class="row">
         <button class="ui column right labeled icon button">
             Forward
-            <i class="right chevron icon"></i>
+            <i className="right chevron icon"></i>
         </button>
 
-        <div class="ui center aligned column">
+        <div className="ui center aligned column">
             Page Number
         </div>
 
-        <button class="ui column labeled icon button">
-            <i class="left chevron icon"></i>
+        <button className="ui column labeled icon button">
+            <i className="left chevron icon"></i>
             Back
         </button>
         </div>
