055372a01795c6dc95ac1aa4ce6a2185f1864051
Hend Jannat <h.jannat@lamah.com>
2021-02-03 15:03:21 +0200

    adding bg color and border color props

--- src/component/login/components/InputField.jsx
+++ src/component/login/components/InputField.jsx
@@ -1,11 +1,11 @@
 import React from 'react';
 
-const InputField = ({title, type, name, placeholder, onChange, icon}) => {
+const InputField = ({title, type, name, placeholder, onChange, icon, color, borderColor}) => {
     return(
         <div className="field">
                 <label>{title}</label>
                 <div className="ui right icon input">
-                    <input  type={type} name={name} placeholder={placeholder} onChange={onChange} />
+                    <input  type={type} name={name} placeholder={placeholder} onChange={onChange}  style={{backgroundColor: color, border: `1px solid ${borderColor}` }} />
                     <i className={icon}></i>
                 </div>
         </div>
