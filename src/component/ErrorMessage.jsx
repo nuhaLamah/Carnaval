5a26a86663b3f3fda058f3e7f512bfbba96a97e2
Hend Jannat <h.jannat@lamah.com>
2021-02-06 15:11:26 +0200

    .

---
+++ src/component/ErrorMessage.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const ErrorMessage = (props)=>{
+    return(
+        <div className="ui negative message">
+        <div className="header">
+        {props.head}
+        </div>
+        <p>{props.content}
+        </p></div>
+    );
+}
+
+export default ErrorMessage;
