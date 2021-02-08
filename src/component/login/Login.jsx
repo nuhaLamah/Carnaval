ef961292aa405d7587437be3b9218c645312fae0
Hend Jannat <h.jannat@lamah.com>
2021-02-08 15:28:22 +0200

    button style

--- src/component/login/Login.jsx
+++ src/component/login/Login.jsx
@@ -50,7 +50,7 @@ const Login = (props) => {
                   
                     <div className="field">
                         <center>
-                        <button className="fluid ui button log-btn" type="submit">دخول</button>
+                        <button className="fluid ui big button log-btn" type="submit">دخول</button>
                         </center>
                     </div>
                     
