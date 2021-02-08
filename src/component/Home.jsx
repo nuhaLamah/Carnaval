1832049fc3b0509b34f3ae50a07058c1ad38656e
Hend Jannat <h.jannat@lamah.com>
2021-02-08 15:23:49 +0200

    width and center

--- src/component/Home.jsx
+++ src/component/Home.jsx
@@ -11,10 +11,11 @@ return (
         <div className="ui section divider"></div>
         <h2 style={{textAlign:'center', fontFamily: 'inherit'}}> Welcome to the System Dashboard </h2>
         {/* --------- Makani validation ------------ */}
-        
+        <center>
         <Link to ={'/QrReader'}><button className="ui button text" type="submit" >Customer</button></Link>
         <Link to ={'/Store'}><button className="ui button text" type="submit" >Store</button></Link>
         <Link to ={'/login'}><button className="ui button text" type="submit" >Login</button></Link>
+        </center>
         </div>
     </div>
 )
