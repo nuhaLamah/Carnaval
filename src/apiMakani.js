cd9e130f25f0dd8a965a4bab3327a481ff445e3b
Hend Jannat <h.jannat@lamah.com>
2021-02-16 14:51:51 +0200

    delete consoles

--- src/apiMakani.js
+++ src/apiMakani.js
@@ -1,13 +1,10 @@
 import axios from "axios";
 
 const API_KEY = '6c4ab7793f8ba95f2587cde26dc426f30e6703fd9a4fedb3a68e5f274b1a90e1';
-const URL = 'http://pas.makani-api.com/public/makani';
+const URL = 'https://pas.makani-api.com/public/makani';
 
 const config = {headers : {'API-KEY': API_KEY}};
 
 export const getLocationInfo = (location)=>{
-   // console.log(URL);
-   // console.log(API_KEY);
-   // console.log(location);
     return axios.get(`${URL}/placeinformation/${location}`, config);
 }
