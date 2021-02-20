afe920de5553a438576a65df8ab490b400e06819
Hend Jannat <h.jannat@lamah.com>
2021-02-20 10:19:59 +0200

    renaming qr scanner route

--- src/App.js
+++ src/App.js
@@ -25,7 +25,7 @@ const App = (props)=> {
          <Route path="/Success/:StoreCode"  component={SuccessReg}/> {/* Success Store Regiteration Page */}
          <Route path="/Success"  component={SuccessReg}/> {/* Success Customer Regiteration Page */}
          <Route path="/QRCode/:code"  component={!props.isLog? DirectToLogin: Print}/> {/* Print QR Code */}
-         <Route path="/QrReader"  component={QrReader} />   {/* QR Reader Page */}
+         <Route path="/Scan"  component={QrReader} />   {/* QR Reader Page */}
          <Route path="/Customer/:storeCode"  component={Customer} />   {/* Customer Regiteration Form */}
          <Route exact path="/website" render={() => (window.location = "https://misratafestival.ly/")} />
 
